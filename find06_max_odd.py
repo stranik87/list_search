9464e96bfd2de0323ea410bde7d90c41925ad01a
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-31 07:42:18 +0300

    find06

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,17 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    x = 0
+    a = 0
+    maxOdd = -1
+    maxNum = [] 
+    while x < len(data):
+        if data[x]%2 == 1:
+            maxNum.append(data[x])
+            a = max(maxNum)
+        elif a%2 != 1:
+            return -1
+        x += 1
+    return maxNum ,a
+
+print(find_max_odd([1, 8, 3, 8, 5]))
